e2b62d23964375828e7e892886562faaa9114f86
Alexey Chekanov <chekanov@hotmail.com>
2018-03-05 15:40:36 -0500

    Initial Commit

---
+++ UIElements/UIElements.h
@@ -0,0 +1,19 @@
+//
+//  UIElements.h
+//  UIElements
+//
+//  Created by Alexey Chekanov on 3/5/18.
+//  Copyright © 2018 Tips & Tricks, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIElements.
+FOUNDATION_EXPORT double UIElementsVersionNumber;
+
+//! Project version string for UIElements.
+FOUNDATION_EXPORT const unsigned char UIElementsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIElements/PublicHeader.h>
+
+
