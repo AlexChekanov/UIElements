6a1c365e11b8740878e6729f1d94459a639b554b
Alexey Chekanov <chekanov@hotmail.com>
2018-03-06 13:37:57 -0500

    File structure rearranged

--- UIElements/Buttons/TwoStatesButton.swift
+++ UIElements/Buttons/TwoStatesButton.swift
@@ -1,6 +1,16 @@
 import UIKit
+import UIExtensions
 
 public final class TwoStatesButton: UIControl {
     
     public let button = UIButton()
+    
+    let v = UIView()
+    
+    func fj() {
+        v.loadFromNib()
+    }
+   
 }
+
+
